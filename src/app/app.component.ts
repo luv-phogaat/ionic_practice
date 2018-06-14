8346df8f4c02915d28eae237f1863f79134c8a35
luv-phogaat <luv-phogaat@users.noreply.github.com>
2018-06-14 12:00:19 +0530

    Add files via upload

---
+++ src/app/app.component.ts
@@ -0,0 +1,26 @@
+import { Component } from '@angular/core';
+import { Platform, ModalController } from 'ionic-angular';
+import { StatusBar } from '@ionic-native/status-bar';
+import { SplashScreen } from '@ionic-native/splash-screen';
+
+import { TabsPage } from '../pages/tabs/tabs';
+import { HomePage } from '../pages/home/home';
+import { Splash } from '../pages/splash/splash';
+
+@Component({
+  templateUrl: 'app.html'
+})
+export class MyApp {
+  rootPage:any = TabsPage;
+
+  constructor(platform: Platform, statusBar: StatusBar, splashScreen: SplashScreen, modalCtrl: ModalController) {
+    platform.ready().then(() => {
+      // Okay, so the platform is ready and our plugins are available.
+      // Here you can do any higher level native things you might need.
+      statusBar.styleDefault();
+      let splash = modalCtrl.create(Splash);
+      splash.present();
+      //splashScreen.hide();
+    });
+  }
+}
