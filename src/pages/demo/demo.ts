a20fa80ec4acc3a4587582690e08e71a5f51565b
luv-phogaat <luv-phogaat@users.noreply.github.com>
2018-06-21 18:07:24 +0530

    Added Side Menu in Demo Page

--- src/pages/demo/demo.ts
+++ src/pages/demo/demo.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { NavController, MenuController } from 'ionic-angular';
+import {App,  NavController, MenuController } from 'ionic-angular';
 
 @Component({
   selector: 'page-demo',
@@ -7,8 +7,64 @@ import { NavController, MenuController } from 'ionic-angular';
 })
 export class DemoPage {
 
-  constructor(public navCtrl: NavController, menu: MenuController) {
+  constructor(public navCtrl: NavController, menu: MenuController, app: App,) {
     menu.enable(true);
   }
 
 }
+@Component({
+  template: `
+<ion-header>
+  <ion-navbar>
+    <button ion-button menuToggle icon-only>
+      <ion-icon name='menu'></ion-icon>
+    </button>
+    <ion-title>
+      Menus
+    </ion-title>
+  </ion-navbar>
+</ion-header>
+<ion-content padding>
+  <button ion-button block menuToggle>Toggle Menu</button>
+</ion-content>
+`
+})
+export class PageOne { }
+
+@Component({
+  template: `
+<ion-header>
+  <ion-navbar>
+    <button ion-button menuToggle icon-only>
+      <ion-icon name='menu'></ion-icon>
+    </button>
+    <ion-title>
+      Friends
+    </ion-title>
+  </ion-navbar>
+</ion-header>
+<ion-content padding>
+  <button ion-button block menuToggle>Toggle Menu</button>
+</ion-content>
+`
+})
+export class PageTwo { }
+
+@Component({
+  template: `
+<ion-header>
+  <ion-navbar>
+    <button ion-button menuToggle icon-only>
+      <ion-icon name='menu'></ion-icon>
+    </button>
+    <ion-title>
+      Events
+    </ion-title>
+  </ion-navbar>
+</ion-header>
+<ion-content padding>
+  <button ion-button block menuToggle>Toggle Menu</button>
+</ion-content>
+`
+})
+export class PageThree { }
